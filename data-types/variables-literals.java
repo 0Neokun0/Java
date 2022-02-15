3669f066d10a261997dd1e98af8ee39e618883a7
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-15 19:39:52 +0900

    Variables Literals Programe

---
+++ data-types/variables-literals.java
@@ -0,0 +1,20 @@
+class Main 
+{
+    public static void main(String[] args)
+    {
+        double myDouble = 3.4;
+        float myFloat = 3.4F;
+        double myDoubleScientific = 3.445e2;
+        boolean flag = true;
+        byte range = 124;
+        char letter1 = 'N';
+
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myDoubleScientific);
+        System.out.println(flag);
+        System.out.println(range);
+        System.out.println(letter1);
+ 
+    }
+}
