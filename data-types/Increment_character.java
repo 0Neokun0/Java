09dbecc562a64a282be0bb45502982c639f51854
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-16 17:59:11 +0900

    Increment Alphaber Programe

---
+++ data-types/Increment_character.java
@@ -0,0 +1,13 @@
+public class Increment_character
+{
+    public static void main(String args[])
+    {
+        char myChar1 = 'A';
+        char myChar2 = 'B';
+
+        System.out.println( "1st Character : " +myChar1 );
+        System.out.println( "2nd Character : " +myChar2 );
+        myChar2++; // Increment Character
+        System.out.println( "Incremente to 3rd Character : " +myChar2 );
+    }
+}
