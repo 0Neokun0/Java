9c17f5fed7cace99e36faf4476b82c400443a7bf
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-17 10:12:47 +0900

    Arithmetic Operator Program

---
+++ operators/arithmetic.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Declare variables
+        int number1 = 10, number2 = 5;
+
+        // Addition
+        System.out.println("10 + 5 = " + (number1 + number2));
+
+        // Subtraction
+        System.out.println("10 - 5 = " + (number1 - number2));
+
+        // Multiplication
+        System.out.println("10 * 5 = " + (number1 * number2));
+
+        // Division
+        System.out.println("10 / 5 = " + (number1 / number2));
+
+        // Module
+        System.out.println("10 % 5 = " + (number1 % number2));
+
+    }
+}
