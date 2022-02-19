7f28ede519f704edaefee6d3e279fae4ad9e4484
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-19 16:56:07 +0900

    Switch Case Program

---
+++ switch-case/switch.java
@@ -0,0 +1,20 @@
+class Main 
+{
+    public static void main(String[] args)
+    {
+        int expression = 2;
+
+        // Switch Statement To Check Size
+        switch (expression) 
+        {
+            case 1:
+                System.out.println("Case 1");
+            case 2:
+                System.out.println("Case 2");
+            case 3:
+                System.out.println("Case 3");
+            case 4:
+                System.out.println("Default case");
+        }
+    }
+}
