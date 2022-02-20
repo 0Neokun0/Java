9352d90e6956e9aba994482ae5eb60ed77630ba1
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-20 19:32:39 +0900

    Java Loops Program

---
+++ loops/loops.java
@@ -0,0 +1,14 @@
+
+class Main {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        // For Loops
+
+        for ( int i = 1; i <= number; ++i) {
+            System.out.println(i);
+        }
+    }
+    
+}
