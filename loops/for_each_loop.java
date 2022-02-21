85c9210735fe0af675e7ab898f3b06904f4dd3a1
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-21 15:50:58 +0900

    For Each Loop Java Program

---
+++ loops/for_each_loop.java
@@ -0,0 +1,13 @@
+// Display all elements in array
+class Main {
+    public static void main(String[] args) {
+
+        char[] elements = {'T', 'H', 'A', 'T', ' ', 'W', 'H', 'I', 'C', 'H', ' ', 'I', 'S', ' ', 'N', 'O', 'T'};
+
+        // Iterating Through An Array Using A For Loop
+
+        for ( int i = 0; i < elements.length; ++i ) {
+            System.out.println(elements[i]);
+        }
+    }
+}
