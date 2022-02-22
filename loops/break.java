933e045fd121eb68f6b4e712b0fa0bac5ea1a2de
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-22 09:27:01 +0900

    Break Statement Java Program

---
+++ loops/break.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) 
+    {
+
+        // For Loop
+        for ( int i = 1; i <= 10; ++i )
+        {
+            // If The Value of i is % Then Loop Terminates
+            if ( i == 5 )
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
