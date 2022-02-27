c1f34b7391ad34dda6ce62cfe8dd6c209a281233
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-27 19:43:08 +0900

    String And Length Java Program

---
+++ string/string_and_length.java
@@ -0,0 +1,13 @@
+class Main 
+{
+    public static void main(String[] args)
+    {
+        // Create A String
+        String greet = "Hello ! World";
+        System.out.println("String: " + greet);
+
+        // Get The Length Of Greet
+        int length = greet.length();
+        System.out.println("Length: " + length);
+    }
+}
