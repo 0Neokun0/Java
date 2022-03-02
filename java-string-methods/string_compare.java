f7f0bc50f46cabda8f21bc308de3854f14ab5565
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-03 08:46:16 +0900

    Compare String Java Program

---
+++ java-string-methods/string_compare.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        String sentence1 = "Learning Everything is Not Possible.";
+        String sentence2 = "Learning Step by Step is Possible.";
+    
+
+    // If sentence1 and sentence2 are Equal, Then The Result is 0
+    if (sentence1.compareTo(sentence2) == 0) {
+        System.out.println("sentence1 and sentence2 are EQUAL");
+    }
+    else {
+        System.out.println("sentence1 and sentence2 are NOT EQUAL ");
+    }
+  }
+}
