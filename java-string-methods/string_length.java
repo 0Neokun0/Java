18db4fd705643c9525f45d34a438d460287c13b9
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-04 17:26:04 +0900

    String Length Java Program

---
+++ java-string-methods/string_length.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String str1 = "Java is Complex";
+
+        System.out.println("String Length :" + str1.length()); // 15
+        System.out.println("String Length :" + "Learn Java".length()); // 10
+    }
+}
