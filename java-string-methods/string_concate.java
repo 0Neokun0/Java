8e69edd4cd07709d946732d3f531ae3308aba612
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-08 16:25:06 +0900

    String Concatetion Java Program

---
+++ java-string-methods/string_concate.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        String str1 = "Learning ";
+        String str2 = "Java";
+
+        // Concatenate str1 And str2
+        System.out.println(str1 + str2); // "Learning Java"
+
+        // Concatenate str2 And str1
+        System.out.println(str2 + str1); // "JavaLearning"
+    }
+}
