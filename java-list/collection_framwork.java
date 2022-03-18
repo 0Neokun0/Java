f3a8814fd33c8523a6413e4a9f1c8711b0270bf3
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-18 18:18:06 +0900

    Collection Method

---
+++ java-list/collection_framwork.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+
+        // Add Elements 
+        animals.add("Goat");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList : " + animals);
+    }
+}
