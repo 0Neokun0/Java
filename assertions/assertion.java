006a0d9bbe8102a57874fc12c270d74d1ad61e06
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-19 18:51:00 +0900

    Assertion Java

---
+++ assertions/assertion.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String ars[]) {
+        String[] weekends = {"Friday", "Saturday", "Sunday"};
+        assert weekends.length == 2;
+        System.out.println("There are " + weekends.length + " weekends in a week");
+    }
+}
