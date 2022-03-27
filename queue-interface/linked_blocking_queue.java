c60bf852699e68e5baf70fe7d480f3b755fc287d
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-27 15:13:10 +0900

    Linked Blocking Queue

---
+++ queue-interface/linked_blocking_queue.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.LinkedBlockingQueue;
+
+class Main {
+    public static void main(String[] args) {
+        LinkedBlockingQueue<String> animals = new LinkedBlockingQueue<>(5);
+
+        // Using add()
+        animals.add("Dog");
+        animals.add("Cat");
+
+        // Using offer()
+        animals.offer("Horse");
+        System.out.println("LinkedBlockingQueue: " + animals);
+    }
+}
