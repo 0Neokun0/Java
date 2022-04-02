868181ad8e282706606b493dc997c7a7c0900fd0
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-03 05:39:27 +0900

    Sum in Loop

---
+++ loops/sum.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 100;
+
+        // For Loop
+        for (int i = 1; i <= num; ++i) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
