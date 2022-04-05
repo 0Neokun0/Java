c2483f08b9f03e6ddfcf90d79b12feb1e97794a7
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-06 07:30:20 +0900

    Joined two String

---
+++ string/two_string.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        // Create First String
+        String first = "Java ";
+        System.out.println("First String : " + first);
+
+        // Create Second String
+        String second = "Programming";
+        System.out.println("Second String : " + second);
+
+        // Join Two Strings
+        String joinedString = first.concat(second);
+        System.out.println("Joined String : " + joinedString);
+    }
+}
