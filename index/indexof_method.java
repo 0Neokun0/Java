e8f63b4bb65837badccb6e851042319d81750fe9
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-08 05:50:24 +0900

    Index Length Method

---
+++ index/indexof_method.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+      String str1 = "Java is fun";
+      int result;
+  
+      // getting index of character 's'
+      System.out.print("String Length is : ");
+      result = str1.indexOf('s');
+  
+      System.out.println(result);
+    }
+  }
