947b2bb062017c921d5d66ebe6c536158015e959
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-26 06:12:41 +0900

    Widening Casting

---
+++ casting/widening_casting.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 9;
+        double double_number = number;
+
+        System.out.println(number);
+        System.out.println(double_number);
+    }
+}
